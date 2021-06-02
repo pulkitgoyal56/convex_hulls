3436586b472825b5ad31a48ad4cf888dea15b926
Pulkit Goyal <pulkit.goyal@jig-saw.co.jp>
2021-06-02 11:29:06 +0900

    change encapsulation order in header files

--- include/geometry/polygon.h
+++ include/geometry/polygon.h
@@ -12,11 +12,6 @@ namespace geometry
 {
     class Polygon
     {
-    private:
-        std::vector<Point> apexes_;
-        double x_lower_bound_, x_upper_bound_;
-        double y_lower_bound_, y_upper_bound_;
-
     public:
         int id_;
         int order_;
@@ -30,6 +25,11 @@ namespace geometry
         bool f2(const Point &point) const; // Checks if point lies inside the polygon.
 
         friend double operator&(const Polygon &polygon_a, const Polygon &polygon_b); // from intersection (∩) in set theory
+
+    private:
+        std::vector<Point> apexes_;
+        double x_lower_bound_, x_upper_bound_;
+        double y_lower_bound_, y_upper_bound_;
     };
 }
 
--- include/geometry/ray.h
+++ include/geometry/ray.h
@@ -12,9 +12,6 @@ namespace geometry
     // A ray is a semi-infinite line which starts from `start_` and extends infinitely towards `end_`
     class Ray : public Line
     {
-    protected:
-        Point start_, end_;
-
     public:
         Ray(){};
         Ray(const Point &start, const Point &end);
@@ -22,6 +19,9 @@ namespace geometry
         virtual bool f2(const Point &point) const; // Checks if point lies on the ray.
 
         friend bool are_intersecting(const Ray &ray_a, const Ray &ray_b);
+
+    protected:
+        Point start_, end_;
     };
 }
 
